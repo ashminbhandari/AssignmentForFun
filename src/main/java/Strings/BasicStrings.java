7432ddbe1ad27f109e8f7af95bb5764c01dc43bc
ashminbhandari <abhandar@ramapo.edu>
2020-11-08 23:34:59 -0500

    #fix test case number 3 for one at a time as well as fin functions

--- src/main/java/Strings/BasicStrings.java
+++ src/main/java/Strings/BasicStrings.java
@@ -3,26 +3,47 @@ package Strings;
 public class BasicStrings {
 
     public String flipConcat(String string1, String string2) {
-        return null;
+        return string2.concat(string1);
     }
 
     public char getChar(String string, int index) {
-        return ' ';
+        return string.charAt(index);
     }
 
     public String iCantSee(String string) {
-        return null;
+        String ret = "";
+        for (int i = 0; i < string.length(); i++) {
+            ret += " ";
+        }
+        return ret;
     }
 
     public String loudAndClear(String string) {
-        return null;
+        return string.toUpperCase();
     }
 
     public String reverseCase(String string) {
-        return null;
+        String ret = "";
+        for (int i = 0; i < string.length(); i++) {
+            if(string.charAt(i) >= 'A' && string.charAt(i) <= 'Z') {
+                ret += Character.toLowerCase(string.charAt(i));
+            } else {
+                ret += Character.toUpperCase(string.charAt(i));
+            }
+        }
+        return ret;
     }
 
     public String oneAtATime(String string1, String string2) {
-        return null;
+        int handA = 0;
+        int handB = 0;
+        String ret = "";
+        while (handA < string1.length() || handB < string2.length()) {
+            if (handA < string1.length()) ret += string1.charAt(handA);
+            if (handB < string2.length()) ret += string2.charAt(handB);
+            handA += 1;
+            handB += 1;
+        }
+        return ret;
     }
 }
